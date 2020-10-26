7f1509490c6c654945e191e4ba59d01aa2256512
kokiyo1030 <kokiyo1030@naver.com>
2020-10-26 19:41:07 +0900

    source

--- app/src/main/java/com/example/mygames/FreeLine.java
+++ app/src/main/java/com/example/mygames/FreeLine.java
@@ -1,12 +1,16 @@
 package com.example.mygames;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
